9792f4e86ba369a850d149082293344504c38ac8
BT_1N3_10 <sdsd@fnxcn.com>
2024-05-29 09:23:54 -0400

    Awesome mouse game

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,37 +2,61 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
 
 public class MyGdxGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture img;
-	private Sound explosion;
+	private Texture mouseImg;
+	private Texture fruitImg;
+	private Array<Rectangle> fruits;
+	private long lastFruitTime;
+	private Sound dingSound;
 	private Music bgm;
 	private OrthographicCamera camera;
 	private Rectangle mouse;
+	private SpriteBatch batch;
 
 	
 	@Override
 	public void create () {
+		// wack
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 960, 540);
+
+		// Images
 		batch = new SpriteBatch();
-		img = new Texture("mouse.png");
+		mouseImg = new Texture(Gdx.files.internal("mouse.png"));
+		fruitImg = new Texture(Gdx.files.internal("fruit.png"));
+		fruits = new Array<Rectangle>();
+		spawnFruit();
+
 		mouse = new Rectangle();
-		mouse.x = 960 / 2 - 64 / 2;
-		mouse.y = 20;
+		mouse.x = 960 / 2 - 256 / 2;
+		mouse.y = 560 / 2 - 256 / 2;
 		mouse.width = 256;
 		mouse.height = 256;
-		explosion = Gdx.audio.newSound(Gdx.files.internal("explosion.mp3"));
+
+		// Sound
 		bgm = Gdx.audio.newMusic(Gdx.files.internal("Memories_of_Spring.mp3"));
 		bgm.setLooping(true);
 		bgm.play();
+
+		dingSound = Gdx.audio.newSound(Gdx.files.internal("ding.mp3"));
+
+
 	}
 
 	@Override
@@ -41,16 +65,54 @@ public class MyGdxGame extends ApplicationAdapter {
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, mouse.x, mouse.y);
+		batch.draw(mouseImg, mouse.x, mouse.y);
+		for(Rectangle fruit: fruits) {
+			batch.draw(fruitImg, fruit.x, fruit.y);
+		}
 		batch.end();
+		for (Iterator<Rectangle> iter = fruits.iterator(); iter.hasNext(); ) {
+			Rectangle raindrop = iter.next();
+			if(raindrop.overlaps(mouse)) {
+				dingSound.play();
+				iter.remove();
+			}
+		}
+
+		if(Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			mouse.x = touchPos.x - 256 / 2;
+			mouse.y = touchPos.y - 256 / 2;
+		}
+
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) mouse.x -= 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) mouse.x += 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)) mouse.y += 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) mouse.y -= 200 * Gdx.graphics.getDeltaTime();
 
 
 
+		if(TimeUtils.nanoTime() - lastFruitTime > 1000000000) spawnFruit();
 	}
 	
 	@Override
 	public void dispose () {
+		fruitImg.dispose();
+		mouseImg.dispose();
+		dingSound.dispose();
+		bgm.dispose();
 		batch.dispose();
-		img.dispose();
 	}
+
+	private void spawnFruit() {
+		Rectangle fruit = new Rectangle();
+		fruit.x = MathUtils.random(0, 800-50);
+		fruit.y = MathUtils.random(0, 640-50);
+		fruit.width = 50;
+		fruit.height = 50;
+		fruits.add(fruit);
+		lastFruitTime = TimeUtils.nanoTime();
+	}
+
 }
